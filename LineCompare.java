d2497831512ff33c9de16183ad4ec7aab9500db8
tech-engg <techsolution.enggn@gmail.com>
2021-05-14 19:55:25 +0530

    display message

---
+++ LineCompare.java
@@ -0,0 +1,11 @@
+package com.linecomapre;
+
+public class LineCompare {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computaion");
+		// TODO Auto-generated method stub
+
+	}
+
+}
