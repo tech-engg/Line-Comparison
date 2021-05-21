fe6a694f9bc928ee02b612eab00ecf7b479982db
tech-engg <techsolution.enggn@gmail.com>
2021-05-21 20:10:41 +0530

    using oops

--- LineCompare.java
+++ LineCompare.java
@@ -1,52 +1,47 @@
 package com.linecomapre;
-import java.util.Scanner;
-
-import java.util.Scanner;
 
 public class LineCompare {
 
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparision Computaion");
+	public int x1, x2, y1, y2; 
 
-		/* creating the object */
-		Scanner myObj = new Scanner(System.in);
-		System.out.println(" Enter the values of co-ordinate ");
+	public LineCompare(int x1, int x2, int y1, int y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
 
-		System.out.println("Enter the value of (x1,y1)=");
-		int x1 = myObj.nextInt();
-		int y1 = myObj.nextInt();
-		System.out.println("Enter the value of (x2,y2)=");
-		int x2 = myObj.nextInt();
-		int y2 = myObj.nextInt();
-
-		double lengthofLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-
-		System.out.println("Enter the value of (x3,y3)=");
-		int x3 = myObj.nextInt();
-		int y3 = myObj.nextInt();
-		System.out.println("Enter the value of (x4,y4)=");
-		int x4 = myObj.nextInt();
-		int y4 = myObj.nextInt();
-
-		double lengthofLine2 = Math.sqrt(Math.pow(x4 -x3, 2) + Math.pow(y4 -y3, 2));
-
-		System.out.println("length of line2 =" + lengthofLine2);
-		System.out.println("length of line1 =" + lengthofLine1);
-		
-		if(lengthofLine1 == lengthofLine2)
-		{
-			System.out.println("the lines are equal");
-		}	
-		else
-		{	
-			System.out.println("the lines are not equal");
-		}
-		
-		if(lengthofLine1 > lengthofLine2)
-			System.out.println("line one is greater");
-		else
-			System.out.println("line two is greater");
 	}
 
+	public double calculateLengthOfLine() {
+		double lengthofLine = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+		return lengthofLine;
+	}
+
+	public static void diffOfLine(double length1, double length2) {
+		if (length1 < length2) {
+			System.out.println("line-1 is less than line-2.");
+		} else if (length2 < length1) {
+			System.out.println("line-2 is less than line-1.");
+		} else {
+			System.out.println("Both line has equal lenth.");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Wellcome to the Line comparision ");
+
+		LineCompare line1 = new LineCompare(6, 8, 9, 1);
+
+		double length1 = line1.calculateLengthOfLine();
+		System.out.println("Length of line-1 = " + length1);
+
+		LineCompare line2 = new LineCompare(12, 13, 5, 3);
+
+		double length2 = line2.calculateLengthOfLine();
+		System.out.println("Length of line-2 = " + length2);
+
+		diffOfLine(length1, length2);
+	}
 }
